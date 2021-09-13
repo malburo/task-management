e6148678a790339f96e6ef265b95bbf919fbe321
malburo <tongquocbao0605@gmail.com>
2021-09-13 22:54:42 +0700

    create components folder

---
+++ src/components/form-control/InputField.tsx
@@ -0,0 +1,48 @@
+import { FormHelperText } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { Controller, UseFormReturn } from 'react-hook-form';
+
+interface InputFieldProps {
+  form: UseFormReturn<any>;
+  name: string;
+  label?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  size?: string;
+  startIcon?: JSX.Element;
+  endIcon?: JSX.Element;
+}
+
+const InputField: React.FC<InputFieldProps> = (props) => {
+  const { form, name, disabled, placeholder } = props;
+  const { errors } = form.formState;
+  const hasError = !!errors[name];
+
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      render={({ field }) => (
+        <FormControl fullWidth variant="outlined" margin="normal" error={hasError}>
+          <OutlinedInput
+            {...field}
+            disabled={disabled}
+            placeholder={placeholder}
+            id={name}
+            sx={{
+              borderRadius: '8px',
+              border: '1px solid #E0E0E0',
+              boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.05)',
+              fontSize: '12px',
+              fontWeight: '500',
+            }}
+          />
+          <FormHelperText>{errors[name]?.message}</FormHelperText>
+        </FormControl>
+      )}
+    />
+  );
+};
+
+export default InputField;
