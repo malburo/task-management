74788402e35cc1c7929e38a9be25a18c2384c1d8
malburo <tongquocbao0605@gmail.com>
2021-09-13 22:53:37 +0700

    create private and public route

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import { RootState } from 'app/store';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route, RouteProps } from 'react-router-dom';
+
+interface PrivateRouteProps extends RouteProps {
+  component: any;
+  layout?: any;
+}
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, layout: Layout, ...rest }) => {
+  const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+  if (!isAuth) {
+    return <Redirect to="/auth/login" />;
+  }
+  return <Route {...rest} render={(props) => <Component {...props} />} />;
+};
+
+export default PrivateRoute;
