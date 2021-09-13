40112528c7c4618e04b50ccfe6ad7725504bca74
malburo <tongquocbao0605@gmail.com>
2021-09-13 22:56:38 +0700

    create auth slice redux toolkit

---
+++ src/features/Auth/index.tsx
@@ -0,0 +1,19 @@
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import NotFound from '../../components/NotFound';
+import Login from './pages/Login';
+import OAuthLogin from './pages/OAuthLogin';
+import Register from './pages/Register';
+
+const AuthFeature: React.FC = () => {
+  const match = useRouteMatch();
+  return (
+    <Switch>
+      <Route exact path={`${match.url}/login`} component={Login} />
+      <Route exact path={`${match.url}/register`} component={Register} />
+      <Route exact path={`${match.url}/oauth/:access_token`} component={OAuthLogin} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+};
+
+export default AuthFeature;
