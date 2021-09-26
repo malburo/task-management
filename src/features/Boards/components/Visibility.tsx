3e644bbf68d32d017f6c3dae00d99fda0fc3b994
malburo <tongquocbao0605@gmail.com>
2021-09-26 09:17:57 +0700

    add Visibility change component

---
+++ src/features/Boards/components/Visibility.tsx
@@ -0,0 +1,112 @@
+import { Box, Grid, Popover, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import LockIcon from '@material-ui/icons/Lock';
+import React, { useState } from 'react';
+import PublicIcon from '@material-ui/icons/Public';
+
+interface VisibilityProps {
+  onChange: (value: boolean) => void;
+}
+
+const Visibility: React.FC<VisibilityProps> = ({ onChange }) => {
+  const [isPrivate, setIsPrivate] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
+  const handleClickPublic = () => {
+    setIsPrivate(false);
+    onChange(false);
+    setAnchorEl(null);
+  };
+  const handleClickPrivate = () => {
+    setIsPrivate(true);
+    onChange(true);
+    setAnchorEl(null);
+  };
+  return (
+    <Box>
+      <Button
+        aria-describedby={id}
+        onClick={handleClick}
+        variant="contained"
+        color="inherit"
+        fullWidth
+        startIcon={isPrivate ? <LockIcon /> : <PublicIcon />}
+        style={{ marginRight: '16px' }}
+      >
+        {isPrivate ? 'Private' : 'Public'}
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        sx={{
+          marginTop: '12px',
+          marginLeft: '8px',
+        }}
+      >
+        <Box p={4}>
+          <Box>
+            <Typography variant="bold2">Visibility</Typography>
+          </Box>
+          <Box>
+            <Typography variant="regular2">Choose who can see to this board.</Typography>
+          </Box>
+          <Box
+            p={4}
+            borderRadius={2}
+            onClick={handleClickPublic}
+            sx={{
+              ':hover': {
+                backgroundColor: '#F2F2F2',
+              },
+            }}
+          >
+            <Grid container alignItems="center">
+              <PublicIcon sx={{ width: '12px', height: '12px', marginRight: '8px' }} />
+              <Typography variant="regular2">Public</Typography>
+            </Grid>
+            <Typography variant="regular1">Anyone on the internet can see this.</Typography>
+          </Box>
+          <Box
+            p={3}
+            borderRadius={2}
+            onClick={handleClickPrivate}
+            sx={{
+              ':hover': {
+                backgroundColor: '#F2F2F2',
+              },
+            }}
+          >
+            <Grid container alignItems="center">
+              <LockIcon sx={{ width: '12px', height: '12px', marginRight: '8px' }} />
+              <Typography variant="regular2">Private</Typography>
+            </Grid>
+            <Typography variant="regular1">Anyone on the internet can see this.</Typography>
+          </Box>
+        </Box>
+      </Popover>
+    </Box>
+  );
+};
+
+export default Visibility;
