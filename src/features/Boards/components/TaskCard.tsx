ba9cfe57de535936ceb850e5dd1f470c035413b5
malburo <tongquocbao0605@gmail.com>
2021-10-30 18:32:53 +0700

    feature update title task and using drafjs write description

--- src/features/Boards/components/TaskCard.tsx
+++ src/features/Boards/components/TaskCard.tsx
@@ -1,8 +1,7 @@
-import { Avatar, AvatarGroup, Box, Card, Chip, Typography } from '@mui/material';
-import CommentIcon from '@mui/icons-material/Comment';
+import { Card, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { ITask } from 'models/task';
-import { useHistory } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const useStyles = makeStyles({
   cover: {
@@ -23,27 +22,31 @@ const useStyles = makeStyles({
 interface TaskCardProps {
   task: ITask;
 }
-
+interface Params {
+  boardId: string;
+}
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
+  const { boardId } = useParams<Params>();
   const classes = useStyles();
   const history = useHistory();
+
   const handleClickTask = () => {
-    history.push('/boards/616bc72720845003622501e9/zxc');
+    history.push(`/boards/${boardId}/tasks/${task._id}`);
   };
   return (
-    <Card sx={{ border: '2px solid #0000000a', marginBottom: '24px' }} onClick={handleClickTask}>
-      <img
-        src="https://scontent.fsgn3-1.fna.fbcdn.net/v/t1.6435-9/245640291_2726439204317433_3912247688081488990_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=FMnKEZNfGtUAX8D6abN&_nc_ht=scontent.fsgn3-1.fna&oh=52298cd7782ab16c1c883b825243130d&oe=6195EEFF"
-        alt="asd"
-        className={classes.cover}
-      />
-      <Typography variant="regular4">{task.title}</Typography>
-      <Box>
+    <div>
+      <Card sx={{ border: '2px solid #0000000a', marginBottom: '12px' }} onClick={handleClickTask}>
+        {task?.coverUrl?.length >= 0 && (
+          <img src={task.coverUrl} alt="task cover" className={classes.cover} draggable="false" />
+        )}
+
+        <Typography variant="regular4">{task.title}</Typography>
+        {/* <Box>
         <Chip label="javascript" className={classes.label} />
         <Chip label="java" className={classes.label} />
         <Chip label="java" className={classes.label} />
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="space-between">
+      </Box> */}
+        {/* <Box display="flex" alignItems="center" justifyContent="space-between">
         <AvatarGroup max={3}>
           <Avatar variant="rounded" src="https://avatars3.githubusercontent.com/u/22362391?v=4" />
           <Avatar variant="rounded" src="https://avatars3.githubusercontent.com/u/22362391?v=4" />
@@ -54,8 +57,9 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
           <CommentIcon style={{ width: 14, height: 14, color: '#BDBDB' }} />
           <Typography variant="regular1">6</Typography>
         </Box>
-      </Box>
-    </Card>
+      </Box> */}
+      </Card>
+    </div>
   );
 };
 
