c41f3b26bd9dbd6a853c6de3ae19b51b34926529
malburo <tongquocbao0605@gmail.com>
2021-11-14 00:05:36 +0700

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import NotFound from 'components/NotFound';
 import AuthFeature from 'features/Auth';
 import BoardFeature from 'features/Boards';
+import Boards from 'features/Boards/pages/Boards';
 import UserFeature from 'features/User';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthRoute from './AuthRoute';
@@ -10,8 +11,8 @@ const RoutesComponent = () => (
   <BrowserRouter>
     <Switch>
       <AuthRoute path="/auth" component={AuthFeature} />
-      <PrivateRoute path="/boards" component={BoardFeature} />
-      <PrivateRoute path="/appchat" component={ChatFeature} />
+      <PrivateRoute exact path="/boards" component={Boards} />
+      <PrivateRoute path="/boards/:boardId" component={BoardFeature} />
       <PrivateRoute path="/profile" component={UserFeature} />
       <Route component={NotFound} />
     </Switch>
