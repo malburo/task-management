ee121894f4bf20df68b43a748a90d6ba479e7bc8
malburo <tongquocbao0605@gmail.com>
2022-04-01 15:30:43 +0700

    feature upload image

---
+++ src/features/Chat/components/UploadImage.tsx
@@ -0,0 +1,42 @@
+import { Box, Button, IconButton } from '@mui/material';
+import messageApi from 'api/messageApi';
+import uploadApi from 'api/uploadApi';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+import UploadIcon from '@mui/icons-material/Upload';
+
+interface IParams {
+  boardId: string;
+  roomId: string;
+}
+
+const UploadImage = () => {
+  const { roomId, boardId } = useParams<IParams>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleChangeAvatar = async (event: any) => {
+    const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'];
+    const imageObj = event.target.files[0];
+    if (!SUPPORTED_FORMATS.includes(imageObj.type)) {
+      toast.error('Type file does not support');
+      return;
+    }
+    let formData = new FormData();
+    formData.append('image', imageObj);
+    const { data } = await uploadApi.upload(formData);
+    console.log(data);
+    const payload = { roomId, content: data.result.secure_url, type: 'IMAGE' };
+    await messageApi.create(payload);
+    event.target.value = null;
+  };
+
+  return (
+    <IconButton aria-label="Emoji" component="label">
+      <UploadIcon />
+      <input type="file" onChange={handleChangeAvatar} hidden />
+    </IconButton>
+  );
+};
+
+export default UploadImage;
