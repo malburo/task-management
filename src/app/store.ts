efe29cb6abd3a27d6c29b7c7d2779605578d7dab
malburo <tongquocbao0605@gmail.com>
2022-04-01 15:35:57 +0700

    update store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from 'features/Auth/authSlice';
 import boardReducer from 'features/Boards/boardSlice';
-import messagesReducer from 'features/Chat/ReduxSlice/MessagesSlice';
-import roomReducer from 'features/Chat/ReduxSlice/RoomSlice';
-import chatUIReducer from 'features/Chat/ReduxSlice/uiSlice';
 import themeReducer from 'themeSlice';
 
 const rootReducer = {
   auth: authReducer,
   board: boardReducer,
-  room: roomReducer,
   theme: themeReducer,
-  message: messagesReducer,
-  chatUI: chatUIReducer,
 };
 
 const store = configureStore({
