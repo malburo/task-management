2bea66683fdd1ec5e26f3ea21185d7f7d237753e
malburo <tongquocbao0605@gmail.com>
2022-04-01 15:33:50 +0700

    create floating connection edge

---
+++ src/features/Workflow/components/FloatingEdge.tsx
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+import { useStore, getBezierPath } from 'react-flow-renderer';
+
+import { getEdgeParams } from './utils.js';
+
+function FloatingEdge({ id, source, target, markerEnd, style }: any) {
+  const sourceNode = useStore(useCallback((store) => store.nodeInternals.get(source), [source]));
+  const targetNode = useStore(useCallback((store) => store.nodeInternals.get(target), [target]));
+
+  if (!sourceNode || !targetNode) {
+    return null;
+  }
+
+  const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(sourceNode, targetNode);
+
+  const d = getBezierPath({
+    sourceX: sx,
+    sourceY: sy,
+    sourcePosition: sourcePos,
+    targetPosition: targetPos,
+    targetX: tx,
+    targetY: ty,
+  });
+
+  return (
+    <>
+      <path id={id} className="react-flow__edge-path" d={d} markerEnd={markerEnd} style={style}></path>
+      <text>
+        <textPath href={`#${id}`} style={{ fontSize: '12px' }} startOffset="80%" textAnchor="middle"></textPath>
+      </text>
+    </>
+  );
+}
+
+export default FloatingEdge;
