8f6c106c33a224d4e3d0145c04dc223be2cf2556
malburo <tongquocbao0605@gmail.com>
2022-05-04 09:48:38 +0700

    refactor notification

--- src/features/Notification/index.tsx
+++ src/features/Notification/index.tsx
@@ -1,26 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable array-callback-return */
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { IconButton, Popover, Typography } from '@mui/material';
+import { Badge, Button, IconButton, Popover, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import notificationApi from 'api/notificationApi';
 import { socketClient } from 'api/socketClient';
 import { RootState } from 'app/store';
 import NotificationCard from 'features/Notification/components/NotificationCard';
+import useNotification from 'hooks/useNotification';
 import { INotification } from 'models/notification';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
 import NotificationToast from './components/NotificationToast';
 
 const NotificationFeature: React.FC = () => {
+  const notiList = useNotification();
   const { currentUser } = useSelector((state: RootState) => state.auth);
   const [notificationList, setNotificationList] = useState<INotification[]>([]);
   const [pagination, setPagination] = useState<any>({ limit: 8, page: 1 });
 
-  const history = useHistory();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const id = open ? 'notification-popover' : undefined;
@@ -62,73 +62,21 @@ const NotificationFeature: React.FC = () => {
   };
 
   useEffect(() => {
-    (() => {
-      socketClient.on('notification:create', (newNotification: INotification) => {
-        setNotificationList((prev) => [newNotification, ...prev]);
-        if (newNotification.type === 'BOARD:ADD_MEMBER')
-          return toast.custom((t) => (
-            <Box onClick={() => history.push(`/boards/${newNotification.boardId}`)}>
-              <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
-                <Typography variant="regular2">
-                  {newNotification.senderId.username} invited you to {newNotification.content.board.title}
-                </Typography>
-              </NotificationToast>
-            </Box>
-          ));
-        if (newNotification.type === 'TASK:DEADLINE_EXPIRED')
-          return toast.custom((t) => (
-            <Box
-              onClick={() =>
-                history.push(`/boards/${newNotification.boardId}/tasks/${newNotification.content.task._id}`)
-              }
-            >
-              <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
-                <Typography variant="regular2">{newNotification.content.task.title} has expired</Typography>
-              </NotificationToast>
-            </Box>
-          ));
-        if (newNotification.type === 'TASK:REMINDER')
-          return toast.custom((t) => (
-            <Box
-              onClick={() =>
-                history.push(`/boards/${newNotification.boardId}/tasks/${newNotification.content.task._id}`)
-              }
-            >
-              <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
-                <Typography variant="regular2">{newNotification.content.task.title} is about to expire</Typography>
-              </NotificationToast>
-            </Box>
-          ));
-        if (newNotification.type === 'TASK:ASSIGN_MEMBER')
-          return toast.custom((t) => (
-            <Box
-              onClick={() =>
-                history.push(`/boards/${newNotification.boardId}/tasks/${newNotification.content.task._id}`)
-              }
-            >
-              <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
-                <Typography variant="regular2">
-                  {newNotification.senderId.username} was assigned {newNotification.content.task.title} to you
-                </Typography>
-              </NotificationToast>
-            </Box>
-          ));
-        if (newNotification.type === 'TASK:REASSIGN_MEMBER')
-          return toast.custom((t) => (
-            <Box
-              onClick={() =>
-                history.push(`/boards/${newNotification.boardId}/tasks/${newNotification.content.task._id}`)
-              }
-            >
-              <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
-                <Typography variant="regular2">
-                  {newNotification.senderId.username} was reassigned {newNotification.content.task.title} to you
-                </Typography>
-              </NotificationToast>
-            </Box>
-          ));
-      });
-    })();
+    socketClient.on('notification:create', (newNotification: INotification) => {
+      setNotificationList((prev) => [newNotification, ...prev]);
+      const noti = notiList.find((noti: any) => noti.type === newNotification.type);
+      if (!noti) return;
+      toast.custom((t) => (
+        <Box onClick={() => noti.onClick(newNotification)}>
+          <NotificationToast sender={newNotification.senderId} time={newNotification.createdAt}>
+            <Typography variant="regular2">{noti.message(newNotification)}</Typography>
+          </NotificationToast>
+        </Box>
+      ));
+    });
+    return () => {
+      socketClient.off('notification:create');
+    };
   }, []);
 
   const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -137,6 +85,15 @@ const NotificationFeature: React.FC = () => {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleClickDeleteAll = async () => {
+    try {
+      const { data } = await notificationApi.deleteAll();
+      setNotificationList([]);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <Box>
@@ -152,7 +109,9 @@ const NotificationFeature: React.FC = () => {
           height: '40px',
         }}
       >
-        <NotificationsIcon />
+        <Badge color="error" badgeContent={notificationList.length}>
+          <NotificationsIcon />
+        </Badge>
       </IconButton>
       <Popover
         id={id}
@@ -177,11 +136,14 @@ const NotificationFeature: React.FC = () => {
               <Box padding="12px">
                 <Typography variant="bold4">Recent notification</Typography>
               </Box>
-              <Box overflow="scroll">
+              <Box>
                 <InfiniteScroll
                   dataLength={notificationList.length}
                   next={fetchMoreData}
-                  hasMore={Math.ceil(pagination.total / pagination.limit) !== pagination.page - 1}
+                  hasMore={
+                    Math.ceil(pagination.total / pagination.limit) !== pagination.page - 1 &&
+                    notificationList.length !== 0
+                  }
                   height="400px"
                   loader={<h4>Loading...</h4>}
                   endMessage={
@@ -191,76 +153,22 @@ const NotificationFeature: React.FC = () => {
                   }
                 >
                   {notificationList.map((notification) => {
-                    if (notification.type === 'BOARD:ADD_MEMBER')
-                      return (
-                        <Box onClick={() => history.push(`/boards/${notification.boardId}`)} key={notification._id}>
-                          <NotificationCard sender={notification.senderId} time={notification.createdAt}>
-                            <Typography variant="regular2">
-                              {notification.senderId.username} invited you to {notification.content.board.title}
-                            </Typography>
-                          </NotificationCard>
-                        </Box>
-                      );
-                    if (notification.type === 'TASK:DEADLINE_EXPIRED')
-                      return (
-                        <Box
-                          onClick={() =>
-                            history.push(`/boards/${notification.boardId}/tasks/${notification.content.task._id}`)
-                          }
-                          key={notification._id}
-                        >
-                          <NotificationCard sender={notification.senderId} time={notification.createdAt}>
-                            <Typography variant="regular2">{notification.content.task.title} has expired</Typography>
-                          </NotificationCard>
-                        </Box>
-                      );
-                    if (notification.type === 'TASK:REMINDER')
-                      return (
-                        <Box
-                          onClick={() =>
-                            history.push(`/boards/${notification.boardId}/tasks/${notification.content.task._id}`)
-                          }
-                          key={notification._id}
-                        >
-                          <NotificationCard sender={notification.senderId} time={notification.createdAt}>
-                            <Typography variant="regular2">
-                              {notification.content.task.title} is about to expire
-                            </Typography>
-                          </NotificationCard>
-                        </Box>
-                      );
-                    if (notification.type === 'TASK:ASSIGN_MEMBER')
-                      return (
-                        <Box
-                          onClick={() =>
-                            history.push(`/boards/${notification.boardId}/tasks/${notification.content.task._id}`)
-                          }
-                          key={notification._id}
-                        >
-                          <NotificationCard sender={notification.senderId} time={notification.createdAt}>
-                            <Typography variant="regular2">
-                              {notification.senderId.username} was assigned {notification.content.task.title} to you
-                            </Typography>
-                          </NotificationCard>
-                        </Box>
-                      );
-                    if (notification.type === 'TASK:REASSIGN_MEMBER')
-                      return (
-                        <Box
-                          onClick={() =>
-                            history.push(`/boards/${notification.boardId}/tasks/${notification.content.task._id}`)
-                          }
-                          key={notification._id}
-                        >
-                          <NotificationCard sender={notification.senderId} time={notification.createdAt}>
-                            <Typography variant="regular2">
-                              {notification.senderId.username} was reassigned {notification.content.task.title} to you
-                            </Typography>
-                          </NotificationCard>
-                        </Box>
-                      );
+                    const noti = notiList.find((noti: any) => noti.type === notification.type);
+                    if (!noti) return;
+                    return (
+                      <Box onClick={() => noti.onClick(notification)} key={notification._id}>
+                        <NotificationCard sender={notification.senderId} time={notification.createdAt}>
+                          <Typography variant="regular2">{noti.message(notification)}</Typography>
+                        </NotificationCard>
+                      </Box>
+                    );
                   })}
                 </InfiniteScroll>
+              </Box>
+              <Box textAlign="center" mt={4}>
+                <Button variant="contained" onClick={handleClickDeleteAll}>
+                  Read all
+                </Button>
               </Box>
             </Box>
           </Box>
